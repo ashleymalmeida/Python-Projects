c6fb50a59dce3273eaeaf7ee4cfe1001507c5def
ashleymalmeida <almeida20082017@gmail.com>
2024-07-31 12:59:57 -0700

    Create Python Project.py

---
+++ Python Project.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+
+string1 = "Paragliding is"
+string2 = "daring"
+string3 = string1 + string2
+
+print(string3) #outputs "Paragliding isdaring"
+
+
+fries = 30
+fries = str(30)
+print(fries)
+
+
+
+
