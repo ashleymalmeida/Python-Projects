58d3da41c0f3e2f4fe9869cb35fd13d842a75a58
ashleymalmeida <almeida20082017@gmail.com>
2024-08-29 13:08:24 -0700

    Creating reading competition

--- Py Project.py
+++ Py Project.py
@@ -1,16 +1,35 @@
-string1 = input("Choose a noun :")
-string2 = input("Choose a plural noun :")
-string3 = input("Choose a noun :")
-string4 = input("Choose a place :")
-string5 = input("Choose an adjective (Describing word):")
+#string1 = input("Choose a noun :")
+#string2 = input("Choose a plural noun :")
+#string3 = input("Choose a noun :")
+#string4 = input("Choose a place :")
+#string5 = input("Choose an adjective (Describing word):")
 
 
-sentence = """Did you know I have a pet {} ?
-He likes to run around and play with all of the {}
-One morning, I woke up and he was wearing a {} for a hat!
-I especially like to take him to the {}
-because he shows off his {} side."""
+#sentence = """Did you know I have a pet {} ?
+#He likes to run around and play with all of the {}
+#One morning, I woke up and he was wearing a {} for a hat!
+#I especially like to take him to the {}
+#because he shows off his {} side."""
 
 
 
-print(sentence.format(string1, string2, string3, string4, string5))
+#print(sentence.format(string1, string2, string3, string4, string5))
+
+
+
+
+
+
+student = 100
+classroom = 30*student
+print(classroom)
+
+school = classroom * 12 
+
+print(school)
+
+goal = 10000  
+print(goal)
+
+pages_per_student = goal / 360
+print(pages_per_student)
